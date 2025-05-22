aec06a82850a20d27575764e983f2f74b0c06456
TuanWoox <z1venttt@gmail.com>
2025-05-22 22:18:40 +0700

    Finish

--- models/booking.js
+++ models/booking.js
@@ -1,24 +1,43 @@
 const mongoose = require("mongoose");
+const { differenceInCalendarDays } = require("date-fns");
 
-const bookingSchema = new mongoose.Schema({
-  createdAt: { type: Date, default: Date.now },
-  startDate: { type: Date, required: true },
-  endDate: { type: Date, required: true },
-  numNights: { type: Number, required: true },
-  numGuests: { type: Number, required: true },
-  cabinPrice: { type: Number, required: true },
-  extrasPrice: { type: Number, default: 0 },
-  totalPrice: { type: Number, required: true },
-  status: {
-    type: String,
-    enum: ["unconfirmed", "confirmed", "checked-in", "checked-out"],
-    default: "unconfirmed",
+const bookingSchema = new mongoose.Schema(
+  {
+    createdAt: { type: Date, default: Date.now },
+    startDate: { type: Date, required: true },
+    endDate: { type: Date, required: true },
+    numGuests: { type: Number, required: true },
+    cabinPrice: { type: Number, required: true },
+    extrasPrice: { type: Number, default: 0 },
+    status: {
+      type: String,
+      enum: ["unconfirmed", "confirmed", "checked-in", "checked-out"],
+      default: "unconfirmed",
+    },
+    hasBreakfast: { type: Boolean, default: false },
+    isPaid: { type: Boolean, default: false },
+    observations: { type: String },
+    cabin: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Cabin",
+      required: true,
+    },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   },
-  hasBreakfast: { type: Boolean, default: false },
-  isPaid: { type: Boolean, default: false },
-  observations: { type: String },
-  cabin: { type: mongoose.Schema.Types.ObjectId, ref: "Cabin", required: true },
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+// Virtual: numNights
+bookingSchema.virtual("numDates").get(function () {
+  return differenceInCalendarDays(this.endDate, this.startDate);
+});
+
+// Virtual: totalPrice
+bookingSchema.virtual("totalPrice").get(function () {
+  return this.cabinPrice + this.extrasPrice;
 });
 
 module.exports = mongoose.model("Booking", bookingSchema);
--- patterns/prototype/bookingPrototype.js
+++ patterns/prototype/bookingPrototype.js
@@ -22,36 +22,31 @@ class BookingPrototype {
 
   // Clone the prototype and modify specific fields
   async clone(
-    newStartDate,
-    newEndDate,
-    newNumNights,
-    newNumGuests,
-    cabinId,
+    startDate,
+    endDate,
+    numGuests,
+    cabinPrice,
+    extrasPrice,
+    hasBreakfast,
+    observations,
     userId,
-    extrasPrice = 0
+    cabinId
   ) {
     const clonedBooking = Object.create(this);
 
     // Modify properties specific to the new booking
-    clonedBooking.startDate = newStartDate || this.startDate;
-    clonedBooking.endDate = newEndDate || this.endDate;
-    clonedBooking.numNights = newNumNights || this.numNights;
-    clonedBooking.numGuests = newNumGuests || this.numGuests;
+    clonedBooking.startDate = startDate || this.startDate;
+    clonedBooking.endDate = endDate || this.endDate;
+    clonedBooking.numGuests = numGuests || this.numGuests;
+    clonedBooking.cabinPrice = cabinPrice;
     clonedBooking.extrasPrice = extrasPrice || this.extrasPrice;
-    clonedBooking.cabinPrice = cabinId
-      ? await this.getCabinPrice(cabinId)
-      : this.cabinPrice;
-    clonedBooking.totalPrice =
-      clonedBooking.cabinPrice + clonedBooking.extrasPrice;
     clonedBooking.status = "confirmed"; // Default status
-    clonedBooking.hasBreakfast = false; // Default breakfast option
+    clonedBooking.hasBreakfast = hasBreakfast;
     clonedBooking.isPaid = false; // Default payment status
-    clonedBooking.observations = this.observations;
-
+    clonedBooking.observations = observations;
     // Fetch the Cabin and User objects by their ids
     clonedBooking.cabin = await Cabin.findById(cabinId);
     clonedBooking.user = await User.findById(userId);
-
     return clonedBooking;
   }
 
